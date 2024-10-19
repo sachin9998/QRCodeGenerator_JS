a638a900b4ba8981d7356a6a4700ef1a28770705
Sachin Alam <sachinalam4216@gmail.com>
2024-10-19 18:09:54 +0530

    fuctionality done

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = document.getElementById("text");
+const result = document.getElementById("result");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const value = text.value;
+
+   https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+
+   var image = document.createElement("img");
+   image.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${value}`;
+
+   result.appendChild(image);
+
+   text.value = null;
+
+});
